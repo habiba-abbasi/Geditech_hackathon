eea1f76f6f3e3f1fe87dc15ed5a46e8bb7a7c558
Habiba Abbasi <habibaabbasi209@gmail.com>
2018-04-07 23:12:01 +0500

    initial commit

---
+++ app/src/main/java/com/hackathon/gaditech/socialLogistic/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.hackathon.gaditech.socialLogistic;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hackathon.gaditech.socialLogistic.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+    public static  int SPLASH_TIME_OUT=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_splash);
+        // calligrapher =new Calligrapher(this);
+        //calligrapher.setFont(this,"Roboto-Thin.ttf",true);
+
+
+        //Font styling
+//        Calligrapher calligrapher=new Calligrapher(SplashActivity.this);
+//        calligrapher.setFont(SplashActivity.this,"Roboto-Thin.ttf",true);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              //  Intent intent= new Intent(SplashActivity.this,HomeActivity.class);
+                //startActivity(intent);
+
+                //close the activity
+                finish();
+            }
+
+        },SPLASH_TIME_OUT);
+    }
+}
